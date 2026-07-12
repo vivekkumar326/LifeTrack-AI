78fdcba669813f1b6e1a572d37dcca3e3af7bb7a
Vivek Kumar <vks1717276@gmail.com>
2026-07-12 23:12:46 +0530

    feat(auth): implement JWT-based user login

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -53,6 +53,72 @@ const register = async (req, res) => {
   }
 };
 
+
+
+// const bcrypt = require("bcrypt");
+const generateToken = require("../utils/jwt");
+// const User = require("../models/User");
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check required fields
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    // Find user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // Compare password
+    const isPasswordMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // Generate JWT
+    const token = generateToken(user._id);
+
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      data: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
 module.exports = {
   register,
+  login,
 };
